f1ff5a20081798d6d646712eda24444a1a23a0d8
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2015-03-18 02:42:29 +0300

    Tell abr

--- compiler/testData/diagnostics/testsWithJsStdLib/dynamicTypes/dynamicCallsWithLambdas.kt
+++ compiler/testData/diagnostics/testsWithJsStdLib/dynamicTypes/dynamicCallsWithLambdas.kt
@@ -5,17 +5,17 @@ fun test(d: dynamic) {
 
     d.foo { x -> }
 
-    d.foo { (x: Int) -> "" }
+    d.foo { x: Int -> "" }
 
     d.foo { x, y -> "" }
 
-    d.foo { (x: String, y: Int) -> "" }
+    d.foo { x: String, y: Int -> "" }
 
-    d.foo { (x, y: Int) -> "" }
+    d.foo { x, y: Int -> "" }
 
-    d.foo { (x: String, y: Int): Int -> <!TYPE_MISMATCH!>""<!> }
+    d.foo(fun (<!UNUSED_PARAMETER!>x<!>: String, <!UNUSED_PARAMETER!>y<!>: Int): Int = <!TYPE_MISMATCH!>""<!> )
 
-    d.foo { String.(x: String, y: Int): Int -> length() }
+    d.foo(fun  String.(<!UNUSED_PARAMETER!>x<!>: String, <!UNUSED_PARAMETER!>y<!>: Int): Int = length() )
 
     d.foo({})
 
@@ -27,7 +27,7 @@ fun test(d: dynamic) {
 
     d.foo(@label ({ x, y -> }))
 
-    d.foo((@label ({ (x, y: Int) -> })))
+    d.foo((@label ({ x, y: Int -> })))
 
     d.foo(({ x -> }))
 
